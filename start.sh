faf96c0dd5edd99ac172df9516627485e1365f53
James <jcoverdale@redev.co.uk>
2021-11-02 15:59:12 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" -o -z "$DESTINATIONS" ]; then
-  echo "Variables DESTINATIONS (host1:port1|host2:port2), VPNADDR, VPNUSER and VPNPASS must be set."; exit;
+  echo "Variables DESTINATIONS (localPort1:host1:destPort1|localPort2:host2:destPort2), VPNADDR, VPNUSER and VPNPASS must be set."; exit;
 fi
 
 export VPNTIMEOUT=${VPNTIMEOUT:-5}
 
 for i in $(echo $DESTINATIONS | tr "|" "\n")
-do
-	host="$(echo $i | cut -d':' -f1)"
-	port="$(echo $i | cut -d':' -f2)"
-	echo "Setting forward for $host:$port"
-	iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $host:$port  
+do	
+	localport="$(echo $i | cut -d':' -f1)"
+	host="$(echo $i | cut -d':' -f2)"
+	destport="$(echo $i | cut -d':' -f3)"
+	echo "Setting forward for $localport -> $host:$port"
+	iptables -t nat -A PREROUTING -p tcp --dport $localport -j DNAT --to-destination $host:$destport  
 done
 
 iptables -t nat -A POSTROUTING -j MASQUERADE

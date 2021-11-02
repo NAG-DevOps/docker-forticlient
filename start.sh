d5a42764d1af707d9a339b708e90f4a3e7458ca1
James <jcoverdale@redev.co.uk>
2021-11-02 14:59:11 +0000

    make the port forwarding generic and not just RDP

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" -o -z "$VPNRDPIP" ]; then
+if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" -o -z "$DESTIP" -o -z "$DESTPORT" ]; then
   echo "Variables VPNRDPIP, VPNADDR, VPNUSER and VPNPASS must be set."; exit;
 fi
 
 export VPNTIMEOUT=${VPNTIMEOUT:-5}
 
-iptables -t nat -A PREROUTING -p tcp --dport 3380 -j DNAT --to-destination  ${VPNRDPIP}:3389
+iptables -t nat -A PREROUTING -p tcp --dport 3380 -j DNAT --to-destination  ${DESTIP}:${DESTPORT}
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 # Setup masquerade, to allow using the container as a gateway

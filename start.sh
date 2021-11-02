95629d0267ca0100e4ac21ef09a894a6dd97432c
James <jcoverdale@redev.co.uk>
2021-11-02 15:44:22 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" -o -z "$DESTIP" -o -z "$DESTPORT" ]; then
-  echo "Variables VPNRDPIP, VPNADDR, VPNUSER and VPNPASS must be set."; exit;
+if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" -o -z "$DESTINATIONS" -o]; then
+  echo "Variables DESTINATIONS (host1:port1|host2:port2), VPNADDR, VPNUSER and VPNPASS must be set."; exit;
 fi
 
 export VPNTIMEOUT=${VPNTIMEOUT:-5}
 
-iptables -t nat -A PREROUTING -p tcp --dport 3380 -j DNAT --to-destination  ${DESTIP}:${DESTPORT}
+for i in $(echo $DESTINATIONS | tr "|" "\n")
+do
+	host="$(echo $i | cut -d':' -f1)"
+	port="$(echo $i | cut -d':' -f2)"	
+	iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $host:$port  
+done
+
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 # Setup masquerade, to allow using the container as a gateway
